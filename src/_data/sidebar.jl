70febf288fde943b0dd2650fccfb3eef4656874a
Jesse E <jesse.d.everington@gmail.com>
2026-04-18 16:34:00 -0400

    refactor sidebar structure to use Dict for consistency and add dropdown styles

--- src/_data/sidebar.jl
+++ src/_data/sidebar.jl
@@ -1,11 +1,11 @@
 [
     "welcome" => "Welcome",
-    "parent1" => [
+    "parent1" => Dict(
         "parent1_mod1" => "Module 1: Adding course material",
         "parent1_mod2" => "Module 2: Quantum Gates"
-    ],
-    "parent2" => [
+    ),
+    "parent2" => Dict(
         "parent2_mod1" => "Module 3: Placeholder",
         "parent2_mod2" => "Module 4: Coming Soon"
-    ]
+    )
 ]
